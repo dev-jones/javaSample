b641b4150bdd7c04a9e6abf106e194b56b565d62
dev-jones <twry29@gmail.com>
2020-11-18 21:16:21 +0900

    컨캣과 어펜드 예제

---
+++ coTeProject/src/concatNappend/Main.java
@@ -0,0 +1,12 @@
+package concatNappend;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		String a = "첫번째 텍스트";
+		String b = "두번째 텍스트";
+		
+		System.out.println(a.concat(b));
+	}
+}
---
+++ coTeProject/src/concatNappend/Main01.java
@@ -0,0 +1,13 @@
+package concatNappend;
+
+public class Main01 {
+
+	public static void main(String[] args) {
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append("두번째 텍스트. ");
+		sb.append("세번째 텍스트.");
+		
+		System.out.println(sb);
+	}
+}
